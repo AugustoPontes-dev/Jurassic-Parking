f39a1ea1e4e2d8fc37983e9273e451881c6c328c
Edmilson Gomes <edmilsonsantosg2@gmail.com>
2025-11-20 12:22:41 -0300

    Inserção da tela de recarga

---
+++ src/app/recarga/recarga.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-recarga',
+  imports: [],
+  templateUrl: './recarga.html',
+  styleUrl: './recarga.css',
+})
+export class Recarga {
+
+}
